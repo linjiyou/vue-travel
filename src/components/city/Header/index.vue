999ed5d3cac9798ddbcf1ef843cc7f6f3ef2813f
linjiyou <13711705166@163.com>
2019-10-16 23:15:05 +0800

    测试提交是否正常

---
+++ src/components/city/Header/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+    <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {},
+  methods: {}
+}
+</script>
+<style lang="stylus" scoped>
+@import "~@/assets/styles/varibles.styl"
+.header{
+  position relative
+  overflow hidden
+  height $headerHeight
+  line-height $headerHeight
+  text-align center
+  color #fff
+  background $bgColor
+  font-size .32rem
+  .header-back{
+   position absolute
+   left 0
+   font-size .4rem
+   width .64rem
+   color #fff
+  }
+}
+</style>
